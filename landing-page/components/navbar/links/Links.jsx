337abb8263b3ab95f1eb8113469e4be5c255a885
Cleve-codes <clevemomanyi@gmail.com>
2024-01-08 16:36:29 +0300

    mobile nav

--- landing-page/components/navbar/links/Links.jsx
+++ landing-page/components/navbar/links/Links.jsx
@@ -1,22 +1,61 @@
+"use client";
 import React from 'react'
-import Link from 'next/link'
+import { useState } from 'react'
 import styles from './links.module.css'
 import NavLink from './navLink/NavLink'
 
-const Links = () => {
+const links = [
+  { title: 'Home', path: '/' },
+  { title: 'About', path: '/about' },
+  { title: 'Contact', path: '/contact' },
+  { title: 'Blog', path: '/blog' },
+]
 
-  const links = [
-    { title: 'Home', path: '/' },
-    { title: 'About', path: '/about' },
-    { title: 'Contact', path: '/contact' },
-    { title: 'Blog', path: '/blog' },
-  ]
+
+const Links = () => {
+  // Mobile Nav State
+  const [openMobileNav, setOpenMobileNav] = useState(false)
+
+  // TEMPORARY
+  const session = true;
+  const isAdmin = true;
 
   return (
+        <div className=''>
     <div className={styles.links}>
       {links.map((link, index) => (
         <NavLink key={index} item={link} />
-      ))}
+        ))}
+      { session ? (
+        <>
+        { isAdmin && <NavLink item={{ title: 'Admin', path: '/admin' }} /> }
+        <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: 'Login', path: '/login' }} />
+        ) }
+    </div>
+    <button onClick={() => setOpenMobileNav(prev => !prev)}>Menu</button>
+
+    {openMobileNav && (
+
+      <div className={styles.mobileLinks}>
+        {
+          links.map((link, index) => (
+          <NavLink key={index} item={link} />
+          ))}
+          { session ? (
+            <>
+            { isAdmin && <NavLink item={{ title: 'Admin', path: '/admin' }} /> }
+            <button className={styles.logout}>Logout</button>
+            </>
+          ) : (
+            <NavLink item={{ title: 'Login', path: '/login' }} />
+            ) }
+        </div>
+
+      )}
+
     </div>
   )
 }
