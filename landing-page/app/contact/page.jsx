04ab45ae5dee5af298519150bac6e66d751dd16b
Cleve-codes <clevemomanyi@gmail.com>
2024-01-08 23:09:44 +0300

    contactPage done

--- landing-page/app/contact/page.jsx
+++ landing-page/app/contact/page.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
+import styles from './contact.module.css'
 
 const page = () => {
   return (
-    <div>Contact page</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="contact" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action='' className={styles.form}>
+            <input type='text' placeholder='Name and Surname' />
+            <input type="text" placeholder='Email Adress' />
+            <input type="text" placeholder='Phone Number (optional)' />
+            <textarea name='' id='' cols='30' rows='10' placeholder='Message' />
+            <button>Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
