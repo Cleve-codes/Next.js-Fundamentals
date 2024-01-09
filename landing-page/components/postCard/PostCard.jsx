740ca861e73162645477424c441bfe6aa838ab80
Cleve-codes <clevemomanyi@gmail.com>
2024-01-09 07:41:41 +0300

    blogPost design

--- landing-page/components/postCard/PostCard.jsx
+++ landing-page/components/postCard/PostCard.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './postcard.module.css'
+
 
 const PostCard = () => {
   return (
-    <div>PostCard</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.imgContainer}>
+          <Image src="/post.png" alt="contact" fill className={styles.img} />
+        </div>
+        <span className={styles.date}>09.01.2024</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>Title</h1>
+        <p className={styles.desc}>DEsc</p>
+        <Link href='/blog/post'>
+          Read More
+        </Link>
+      </div>
+    </div>
   )
 }
 
