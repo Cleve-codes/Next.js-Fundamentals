8b98fb2288503a08bb79d50b50c60615196dd2bd
Cleve-codes <clevemomanyi@gmail.com>
2024-01-10 05:46:02 +0300

    mongoDb

--- landing-page/components/postUser/PostUser.jsx
+++ landing-page/components/postUser/PostUser.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import styles from './postUser.module.css'
+import { getUser } from '../../lib/data'
 
-const getData = async(userId) => {
-    const data = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, {cache: "no-store"})
+// const getData = async(userId) => {
+//     const data = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, {cache: "no-store"})
 
-    if(!data.ok) {
-        throw new Error("Something went wrong")
-    }
+//     if(!data.ok) {
+//         throw new Error("Something went wrong")
+//     }
 
-    return data.json()
-}
+//     return data.json()
+// }
 
 const PostUser = async ({userId}) => {
 
-  const user = await getData(userId)
+  // const user = await getData(userId)
 
+  const user = await getUser(userId)
 
   return (
     <div className={styles.container}>
--- landing-page/lib/data.js
+++ landing-page/lib/data.js
@@ -1,25 +1,66 @@
-const users = [
-  { id: 1, name: 'John Doe', email: 'johndoegmail.com' },
-  { id: 2, name: 'Jane Doe', email: 'janedoegmail.com' },
-  { id: 3, name: 'Jim Doe', email: 'jimdoegmail.com' },
-  { id: 4, name: 'Josh Doe', email: 'joshdoegmail.com'}
-]
+// const users = [
+//   { id: 1, name: 'John Doe', email: 'johndoegmail.com' },
+//   { id: 2, name: 'Jane Doe', email: 'janedoegmail.com' },
+//   { id: 3, name: 'Jim Doe', email: 'jimdoegmail.com' },
+//   { id: 4, name: 'Josh Doe', email: 'joshdoegmail.com'}
+// ]
 
-const posts = [
-  { id: 1, title: 'Lorem Ipsum', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna. Sed euismod, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna.', userId: 1 },
-  { id: 2, title: 'Lorem Ipsum', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna. Sed euismod, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna.', userId: 1 },
-  { id: 3, title: 'Lorem Ipsum', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna. Sed euismod, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna.', userId: 2 },
-  { id: 4, title: 'Lorem Ipsum', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna. Sed euismod, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna.', userId: 3 },
-]
+
+// const posts = [
+//   { id: 1, title: 'Lorem Ipsum', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna. Sed euismod, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna.', userId: 1 },
+//   { id: 2, title: 'Lorem Ipsum', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna. Sed euismod, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna.', userId: 1 },
+//   { id: 3, title: 'Lorem Ipsum', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna. Sed euismod, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna.', userId: 2 },
+//   { id: 4, title: 'Lorem Ipsum', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna. Sed euismod, nisl eget ultricies aliquam, nunc sapien ultricies nunc, eget ultricies nisl nisl eget urna.', userId: 3 },
+// ]
+
+import { Post, User } from "./models";
+import { connectToDb } from "./utils";
 
 export const getPosts = async () => {
-  return posts;
+  try{
+    connectToDb()
+    const posts = await Post.find()
+    console.log(posts)
+    return posts;
+  } catch(err) {
+    console.log(err)
+    throw new Error("Failed to find posts!")
+  }
 }
 
-export const getPost = async (id) => {
-  return posts.find(post => post.id === parseInt(id));
+export const getPost = async (slug) => {
+
+  try{
+    connectToDb();
+    const post = await Post.findOne({slug})
+    return post
+  } catch(err) {
+    console.log(err);
+    throw new Error('Failed to fetch Post')
+  }
+
+}
+
+export const getUsers = async () => {
+  try {
+    connectToDb()
+    const users = await User.find()
+    return users;
+  } catch(err) {
+    console.log(err)
+    throw new Error("Failed to fetch Users")
+  }
 }
 
 export const getUser = async (id) => {
-  return users.find(user => user.id === id);
+
+  try{
+    connectToDb();
+    const user = User.findById(id)
+    return user
+  } catch(err){
+    console.log(err)
+    throw new Error("Failed to fetch users")
+  }
+
 }
