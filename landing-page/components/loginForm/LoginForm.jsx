df542be3c7410e2a6d930bea8d53dca3ad5663f6
Cleve-codes <clevemomanyi@gmail.com>
2024-01-11 22:23:57 +0300

    middleware:

--- landing-page/components/loginForm/LoginForm.jsx
+++ landing-page/components/loginForm/LoginForm.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useFormState } from 'react-dom'
 import { login } from '../../lib/action';
 import styles from './login.module.css'
@@ -9,15 +9,7 @@ import Link from 'next/link';
 
 const LoginForm = () => {
 
-  const router = useRouter()
   const [state, formAction] = useFormState(login, undefined)
-
-  useEffect(() => {
-    if(state?.success) {
-    return router.push('/about');
-    }
-  }, [state?.success, router])
-
 
   return (
         <form action={formAction} className={styles.form}>
