69b41a558cb8a3013b46bebcf91421f59d0805a9
Cleve-codes <clevemomanyi@gmail.com>
2024-01-11 14:43:18 +0300

    hashed

--- landing-page/lib/action.js
+++ landing-page/lib/action.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { Post, User } from "./models";
 import { connectToDb } from "./utils";
 import { signIn, signOut } from "./auth";
+import bcrypt from "bcrypt";
 
 export const addPost = async (formData) => {
   "use server"
@@ -64,10 +65,13 @@ export const register = async (formData) => {
 
     if(user) { return "User already exists"}
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       img
     })
 
