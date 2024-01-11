2619b39d6f95507bc7f2ae04acc1d9b06ac81a53
Cleve-codes <clevemomanyi@gmail.com>
2024-01-11 12:12:35 +0300

    github auth

--- landing-page/lib/auth.js
+++ landing-page/lib/auth.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
-export const  {handlers, auth, signIn, signOut} = NextAuth({providers: [Github({
+export const  {handlers: {GET, POST}, auth, signIn, signOut} = NextAuth({providers: [Github({
   clientId: process.env.GITHUB_ID,
   clientSecret: process.env.GITHUB_SECRET
 })
