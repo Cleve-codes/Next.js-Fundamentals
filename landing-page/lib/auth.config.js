bae5212b726777efeb2e37a33b40b54808b4d49d
Cleve-codes <clevemomanyi@gmail.com>
2024-01-11 23:05:37 +0300

    protected routes

--- landing-page/lib/auth.config.js
+++ landing-page/lib/auth.config.js
@@ -4,8 +4,43 @@ export const authConfig = {
   },
   providers: [],
   callbacks: {
+    async jwt({token, user}){
+      if(user){
+        token.id = user.id
+        token.isAdmin = user.isAmin
+      }
+      return token;
+    },
+    async session({session, token}){
+      if (token) {
+        session.user.id = token.id
+        session.user.isAdmin = token.isAdmin
+      }
+      return session;
+    },
     authorized({auth, request}) {
-      return false;
-    }
+      const user = auth?.user;
+      const isOnAdminPanel = request.nextUrl?.startsWith.pathname("/admin");
+      const isOnBlogPage = request.nextUrl?.startsWith.pathname("/blog");
+      const isOnLoginPage = request.nextUrl?.startsWith.pathname('/login');
+
+
+      //Only admin can access admin panel
+      if(isOnAdminPanel && !user?.isAdmin) {
+        return false;
+      }
+
+      // Only authenticated users can access blog page
+      if(isOnBlogPage && !user) {
+        return false;
+      }
+
+
+      // Only unauthenticated users can access login page
+      if(isOnLoginPage && user) {
+        return Response.redirect(new URL("/", request.nextUrl))
+      }
+      return true;
+    },
   },
-}
+};
